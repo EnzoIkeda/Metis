97f10a240f69fd0f004947eff71be11eafad7a47
Enzo Ikeda <enzo.ikeda@protonmail.com>
2026-09-18 19:45:00 -0300

    Encapsular campos publicos de PlacementManager e MainMenuManager, unificar classificacao de tom de evento

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -1,27 +1,28 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 // Bridge entre o espaco de mundo e o grid.
 public class PlacementManager : MonoBehaviour
 {
-    public int width, height;
-
-    [SerializeField] private float cellSize = 1f;
+    [FormerlySerializedAs("width")] [SerializeField] private int _width;
+    [FormerlySerializedAs("height")] [SerializeField] private int _height;
+    [FormerlySerializedAs("cellSize")] [SerializeField] private float _cellSize = 1f;
 
     // Usados so nas fases 2+ de uma rodada, pra sortear o conteudo dos quarteiroes por cima da malha de ruas fixa.
     [SerializeField] private StructureData[] _proceduralBuildingOptions;
     [SerializeField] private StructureData[] _proceduralGreenLotOptions;
 
-    Grid placementGrid;
+    private Grid _placementGrid;
 
     // Instancia pura do grid, exposta pra outros sistemas consultarem o layout.
-    public Grid Grid => placementGrid;
+    public Grid Grid => _placementGrid;
 
     private readonly Dictionary<Vector3Int, StructureData> _placedStructureData = new Dictionary<Vector3Int, StructureData>();
 
     private void Awake()
     {
-        placementGrid = new Grid(width, height);
+        _placementGrid = new Grid(_width, _height);
         RegisterInitialOccupants();
     }
 
@@ -42,7 +43,7 @@ public class PlacementManager : MonoBehaviour
                 continue;
             }
 
-            placementGrid[position.x, position.z] = occupant.CellType;
+            _placementGrid[position.x, position.z] = occupant.CellType;
 
             var isBlockCell = occupant.CellType == CellType.Structure || occupant.CellType == CellType.SpecialStructure;
             if (isBlockCell && isNewPhase)
@@ -64,7 +65,7 @@ public class PlacementManager : MonoBehaviour
     private void RegenerateBlocks()
     {
         var generator = new CityLayoutGenerator(_proceduralBuildingOptions, _proceduralGreenLotOptions);
-        foreach (var cell in generator.Generate(placementGrid))
+        foreach (var cell in generator.Generate(_placementGrid))
         {
             var position = new Vector3Int(cell.X, 0, cell.Z);
             PlaceTemporaryStructure(position, cell.Structure.Prefab, cell.Structure.FootprintCellType, cell.FacingDegrees);
@@ -74,7 +75,7 @@ public class PlacementManager : MonoBehaviour
 
     internal bool CheckIfPositionInBound(Vector3Int position)
     {
-        if(position.x >= 0 && position.x < width && position.z >=0 && position.z < height)
+        if(position.x >= 0 && position.x < _width && position.z >=0 && position.z < _height)
         {
             return true;
         }
@@ -88,12 +89,12 @@ public class PlacementManager : MonoBehaviour
 
     private bool CheckIfPositionIsOfType(Vector3Int position, CellType type)
     {
-        return placementGrid[position.x, position.z] == type;
+        return _placementGrid[position.x, position.z] == type;
     }
 
     internal void PlaceTemporaryStructure(Vector3Int position, GameObject structurePrefab, CellType type, float yRotationDegrees = 0f)
     {
-        placementGrid[position.x, position.z] = type;
+        _placementGrid[position.x, position.z] = type;
         CreateANewStructureModel(position, structurePrefab, type, yRotationDegrees);
     }
 
@@ -110,12 +111,12 @@ public class PlacementManager : MonoBehaviour
     // Converte uma celula pra posicao local, reaproveitavel por qualquer GameObject filho do grid.
     public Vector3 CellToLocalPosition(Vector3Int cell)
     {
-        return new Vector3(cell.x * cellSize, cell.y, cell.z * cellSize);
+        return new Vector3(cell.x * _cellSize, cell.y, cell.z * _cellSize);
     }
 
     private Vector3Int LocalPositionToCell(Vector3 localPosition)
     {
-        return new Vector3Int(Mathf.RoundToInt(localPosition.x / cellSize), 0, Mathf.RoundToInt(localPosition.z / cellSize));
+        return new Vector3Int(Mathf.RoundToInt(localPosition.x / _cellSize), 0, Mathf.RoundToInt(localPosition.z / _cellSize));
     }
 
     public bool PlaceStructure(Vector3Int position, StructureData structureData)
@@ -139,9 +140,9 @@ public class PlacementManager : MonoBehaviour
     public bool TryGetRandomFreePosition(out Vector3Int position)
     {
         var freePositions = new List<Vector3Int>();
-        for (var x = 0; x < width; x++)
+        for (var x = 0; x < _width; x++)
         {
-            for (var z = 0; z < height; z++)
+            for (var z = 0; z < _height; z++)
             {
                 var candidate = new Vector3Int(x, 0, z);
                 if (CheckIfPositionIsFree(candidate))
@@ -163,9 +164,9 @@ public class PlacementManager : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        for (var x = 0; x <= width; x++)
-            Gizmos.DrawLine(transform.position + new Vector3(x * cellSize, 0, 0), transform.position + new Vector3(x * cellSize, 0, height * cellSize));
-        for (var z = 0; z <= height; z++)
-            Gizmos.DrawLine(transform.position + new Vector3(0, 0, z * cellSize), transform.position + new Vector3(width * cellSize, 0, z * cellSize));
+        for (var x = 0; x <= _width; x++)
+            Gizmos.DrawLine(transform.position + new Vector3(x * _cellSize, 0, 0), transform.position + new Vector3(x * _cellSize, 0, _height * _cellSize));
+        for (var z = 0; z <= _height; z++)
+            Gizmos.DrawLine(transform.position + new Vector3(0, 0, z * _cellSize), transform.position + new Vector3(_width * _cellSize, 0, z * _cellSize));
     }
 }
